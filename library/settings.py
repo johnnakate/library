7a1ef430a2aed63663689895af8f1d654326cb16
Johnna Kate dela Vega <72488464+johnnakate@users.noreply.github.com>
2022-02-16 22:15:48 +0800

    Add files via upload

--- library/settings.py
+++ library/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-w4s_1s-px!t65k__jg!z3jkq4l(aih&do109d3vzlb!^)(!!tk'
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
+#SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = False
 
 # SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default=False, cast=bool)
