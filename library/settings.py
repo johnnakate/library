760dccda2e13f23d1591e3d49ab99bdbaf76a56e
Johnna Kate dela Vega <72488464+johnnakate@users.noreply.github.com>
2022-02-22 12:36:11 +0800

    Add files via upload

--- library/settings.py
+++ library/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'libtech',
-        'USER': 'libtechadmin@libtech',
+        'USER': 'libtechadmin',
         'PASSWORD': '*Qwerty123',
         'HOST': 'libtech.mysql.database.azure.com',
         'PORT': '3306',
