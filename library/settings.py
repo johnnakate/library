9c9810919ba85c726e9c5bfa893255d0fe4fa9fc
janeysaeng <mjanepana@gmail.com>
2022-02-23 11:54:58 +0800

    last najud yawa

--- library/settings.py
+++ library/settings.py
@@ -213,7 +213,7 @@ STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
--- libtech/models.py
+++ libtech/models.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls.base import reverse_lazy
-from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.models import Token
 
 # Create your models here.
 
@@ -173,10 +173,10 @@ class RoomReservation(models.Model):
         db_table = "RoomReservation"
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
 
 # PLEASE DONT DELETE
 # jus a copy of reservation form
